5c5dea80696b6a095a975588d45b8177282fcdb5
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-10-08 17:20:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ Config.AUTH_USERS = [6440253535]
 Config.API_ID = 3847632
 Config.API_HASH = "1a9708f807ddd06b10337f2091c67657"
 Config.BOT_TOKEN = "6431767198:AAGCX8GtdDRxbX3WOkDHkQxqyo64-BS-BiA"
-Config.REDIS_HOST = "redis-16727.c16.us-east-1-2.ec2.cloud.redislabs.com:16727"
+Config.REDIS_HOST = "redis-16727.c16.us-east-1-2.ec2.cloud.redislabs.com"
 Config.REDIS_PASS = "7rcBuuSMeVtbxxeVD3zUEpr6ZS2YmLcK"
 REDIS_PORT = "16727"
 #.
