562837e9fca6a979571b09c71b7ae6777001809b
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-10-09 18:25:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
   REDIS_HOST = os.environ.get("REDIS_HOST")
  # REDIS_PORT = int(os.environ.get("REDIS_PORT", 12345))
   REDIS_PASS = os.environ.get("REDIS_PASS")
-  DOWNLOAD_LOCATION = "downloads"
+  DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "/app/downloads")
 
 Config.AUTH_USERS = [6440253535]
 Config.API_ID = 3847632
@@ -23,6 +23,7 @@ Config.BOT_TOKEN = "6431767198:AAGCX8GtdDRxbX3WOkDHkQxqyo64-BS-BiA"
 Config.REDIS_HOST = "redis-19153.c10.us-east-1-2.ec2.cloud.redislabs.com"
 Config.REDIS_PASS = "f9Hstc2xMAI2FMGuLbGsn446LwsTM4c0"
 REDIS_PORT = "19153"
+Config.DOWNLOAD_LOCATION =  "/app/downloads"
 #.
 
 #.
