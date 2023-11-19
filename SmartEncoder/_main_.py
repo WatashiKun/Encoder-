0d4a5bc65c120f3a4869343913901e0e4082d0a4
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-11-19 15:28:56 +0530

    Update _main_.py

--- SmartEncoder/_main_.py
+++ SmartEncoder/_main_.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram import *
+from pyrogram import enums
 
 logging.basicConfig(
     level=logging.DEBUG,
