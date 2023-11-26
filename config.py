5987838bf96c2c428e6f6e11d47f7475741bb529
WatashiKun <152046420+WatashiKun@users.noreply.github.com>
2023-11-26 10:08:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,10 +16,10 @@ class Config(object):
   REDIS_PASS = os.environ.get("REDIS_PASS")
   DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "/app/downloads")
 
-Config.AUTH_USERS = [6748415360]
+Config.AUTH_USERS = [5385471287]
 Config.API_ID = 3847632
 Config.API_HASH = "1a9708f807ddd06b10337f2091c67657"
-Config.BOT_TOKEN = "6312969509:AAGAk_Qj_vS7spkPQnLFY9ohqN1ieO2NYLk"
+Config.BOT_TOKEN = "6200331513:AAFcjwJdhu9zt4xje1MWod1LhvkXhHJ_3cM"
 Config.REDIS_HOST = "redis-19153.c10.us-east-1-2.ec2.cloud.redislabs.com"
 Config.REDIS_PASS = "f9Hstc2xMAI2FMGuLbGsn446LwsTM4c0"
 REDIS_PORT = "19153"
